365d02b3ccfc8b6a3f29435d4ffbb30171fe94c5
MarkelUribe <uribemarkel@gmail.com>
2024-02-09 16:24:26 +0100

    weno

---
+++ push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muribe-l <muribe-l@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/09 16:11:05 by muribe-l          #+#    #+#             */
+/*   Updated: 2024/02/09 16:23:43 by muribe-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP
+# define PUSH_SWAP
+
+# include<unistd.h>
+# include<stdlib.h>
+# include<limits.h>
+
+typedef struct stacks
+{
+
+} stacks;
+
+#endif
