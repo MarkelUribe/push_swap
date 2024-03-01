ed7d2cc75370a821fbc4e31e0834439bdc51cd8c
MarkelUribe <uribemarkel@gmail.com>
2024-03-01 16:26:42 +0100

    kostue kalkulatzen

--- turk.c
+++ turk.c
@@ -6,13 +6,28 @@
 /*   By: muribe-l <muribe-l@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 17:23:24 by muribe-l          #+#    #+#             */
-/*   Updated: 2024/02/29 19:04:54 by muribe-l         ###   ########.fr       */
+/*   Updated: 2024/03/01 16:26:17 by muribe-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack	get_distance(t_ps *l, t_stack *target)
+int	get_distance(t_stack *stack, t_stack *target)
+{
+	int	distance;
+	int	pos;
+	int	size;
+
+	pos = get_pos(stack, target);
+	size = stacksize(stack);
+	if (size / 2 < pos)
+		distance = pos;
+	else
+		distance = size - pos;
+	return (distance);
+}
+
+t_stack	*get_next_smallest(t_stack *stack, t_stack *node)
 {
 
 }
@@ -21,14 +36,13 @@ void	calc_cost(t_ps *l)
 {
 	t_stack	*tmp;
 	int		cost;
-	int		apos;
-	int		bpos;
 
 	tmp = l->a;
 	while (tmp)
 	{
 		cost = 0;
-		apos = get_pos(l->a, tmp);
+		tmp->cost = get_distance(l->a, tmp) +
+			get_distance(l->b, get_next_smallest(l->b, tmp));
 		tmp = tmp->next;
 	}
 }
@@ -37,5 +51,8 @@ void	turk_sort(t_ps *l)
 {
 	pb(l);
 	pb(l);
-	calc_cost(l);
+	while (stacksize(l->a) > 3)
+	{
+		calc_cost(l);
+	}
 }
